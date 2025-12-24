141b8a7d5dade15c0d0290cb9cc5b6d76c490e36
SyedArbaazHussain <arbaaz14122002@gmail.com>
2025-12-24 15:09:23 +0530

    some more refactor

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,15 @@
-import { type Config } from "drizzle-kit";
+import type { Config } from "drizzle-kit";
 
 import { env } from "@/env";
 
 export default {
-  schema: "./src/server/db/schema.ts",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
-  tablesFilter: ["ConnectionUnlimited_*"],
-  migrations: {
-    table: "__drizzle_migrations_connectionunlimited__",
-  },
+	schema: "./src/server/db/schema.ts",
+	dialect: "postgresql",
+	dbCredentials: {
+		url: env.DATABASE_URL,
+	},
+	tablesFilter: ["ConnectionUnlimited_*"],
+	migrations: {
+		table: "__drizzle_migrations_connectionunlimited__",
+	},
 } satisfies Config;
